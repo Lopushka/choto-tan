9eb41e34e1635cbe98c997400ca837d9bdcb08b9
Lopushka <sv13yat@gmail.com>
2022-11-27 21:57:00 +0300

    add ex_05_master.

---
+++ ex_05/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Имя мне напиши!");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "lopushka")
+{
+    Console.WriteLine("привет мастер!");
+}
+else
+{
+    Console.Write("привет, ");
+    Console.WriteLine(username);
+}
