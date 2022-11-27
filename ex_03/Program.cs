cb5f552b76da068582637d49bf9375c606d14c4d
Lopushka <sv13yat@gmail.com>
2022-11-27 21:38:14 +0300

    add Random_sum

--- ex_03/Program.cs
+++ ex_03/Program.cs
@@ -1,4 +1,6 @@
-﻿int numderA = 30;
-int numderB = 5;
+﻿int numderA = new Random().Next(1,10); //1,2,3,4,....9
+Console.WriteLine(numderA);
+int numderB = new Random().Next(1,10);
+Console.WriteLine(numderB);
 int result = numderA + numderB;
 Console.WriteLine(result);
